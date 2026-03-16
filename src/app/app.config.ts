fbfc7d271c811596ba4f0eae4b4eb69b78e46c73
byte-ranajit <ranajitkhandual@gmail.com>
2026-03-17 02:59:50 +0530

    Fix auth DI by configuring HttpClient and JWT interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { Jwt } from './interceptors/jwt';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: Jwt,
+      multi: true,
+    },
   ]
 };
